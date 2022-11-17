f4cdc6481c2c6df20df2ca4b0d75868fa4917987
wahyou <wahyukrisnaaji32@gmail.com>
2022-11-17 15:32:09 +0700

    finish machine learning model

--- Messy_Clean_Rooms_Detection/app.py
+++ Messy_Clean_Rooms_Detection/app.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing import image
 from fastapi import FastAPI, UploadFile
+import io
 import uvicorn
 
 
@@ -12,9 +15,18 @@ async def index():
     return {"Message": "This is Index"}
 
 @app.post('/predict')
-async def predic(file: UploadFile):
-    image = file.filename
-    classes = MODEL.predict(image)
+async def predic(file: UploadFile) -> str:
+    upload = await file.read()
+    content = io.BytesIO(upload)
+
+
+    img = image.load_img(content, target_size=(150,150))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    images = np.vstack([x])
+
+    classes = MODEL.predict(images)
+
     if classes==0:
         return "Kamar rapi"
     else:
